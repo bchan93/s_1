6caf965baf8bde24ed8aac59ac644f165bfe6ee1
Renee Slamka <renee.slamka@gmail.com>
2014-11-10 17:06:37 -0800

    test

--- gen/com/example/sprint1/R.java
+++ gen/com/example/sprint1/R.java
@@ -31,10 +31,12 @@ public final class R {
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int activity_player_sync=0x7f030002;
     }
     public static final class menu {
         public static final int display_message=0x7f070000;
         public static final int main=0x7f070001;
+        public static final int player_sync=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050003;
@@ -44,6 +46,7 @@ public final class R {
         public static final int hello_world=0x7f050006;
         public static final int title_activity_display_message=0x7f050005;
         public static final int title_activity_main=0x7f050004;
+        public static final int title_activity_player_sync=0x7f050007;
     }
     public static final class style {
         /** 
